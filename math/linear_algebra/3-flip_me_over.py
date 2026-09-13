a2002f11a18a620818b89ae5bf780a51428967f5
IshimweJ-Ona <j.ishimwe4@alustudent.com>
2026-09-13 02:43:18 -0700

    updates

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Module that returns the transpose of a 2D matrix."""
 
-def matrix_transport(matrix):
+
+def matrix_transpose(matrix):
     return [list(row) for row in zip(*matrix)]
