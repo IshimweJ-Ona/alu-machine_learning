3ae8c01650f5e8a0b22785ed6211ccd34f55068a
IshimweJ-Ona <j.ishimwe4@alustudent.com>
2026-09-20 22:25:58 +0200

    New folder adv_linear_algebra

---
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import numpy as np
+
+def definiteness(matrix):
+    if not isinstance(matrix, np.ndarray):
+        raise TypeError("matrix must be a numpy.ndarray")
+    if (matrix.dim !=2 or matrix.size == 0
+            or matrix.shape[0] != matrix.shape[1]):
+        return None
+    if not np.allclose(matrix, matrix.T):
+        return None
+
+    w = np.linalg.eigvalsh(matrix)
+    tol = 1e-10
+    n = len(w)
+    pos = np.sum(w > tol)
+    neg = np.sum( w < -tol)
+
+    if pos == n:
+        return "Positive definite"
+    if neg == n:
+        return "Negative definite"
+    if pos == 0:
+        return "Positive semi-definite"
+    if neg == 0:
+        return "Negative semi-definite"
+    return "Indefinite"
