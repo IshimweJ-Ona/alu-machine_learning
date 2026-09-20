5c86f9fe74edbaa03fbbf94ab015a7025c6cdab2
IshimweJ-Ona <j.ishimwe4@alustudent.com>
2026-09-20 13:48:51 -0700

    Updates

--- math/advanced_linear_algebra/5-definiteness.py
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
+"""Module for calculating the definiteness of a matrix"""
 import numpy as np
 
+
 def definiteness(matrix):
+    """Calculates the definiteness of a matrix"""
     if not isinstance(matrix, np.ndarray):
         raise TypeError("matrix must be a numpy.ndarray")
-    if (matrix.dim !=2 or matrix.size == 0
+    if (matrix.ndim != 2 or matrix.size == 0
             or matrix.shape[0] != matrix.shape[1]):
         return None
     if not np.allclose(matrix, matrix.T):
@@ -14,14 +17,15 @@ def definiteness(matrix):
     tol = 1e-10
     n = len(w)
     pos = np.sum(w > tol)
-    neg = np.sum( w < -tol)
+    neg = np.sum(w < -tol)
 
     if pos == n:
         return "Positive definite"
     if neg == n:
         return "Negative definite"
-    if pos == 0:
-        return "Positive semi-definite"
     if neg == 0:
+        return "Positive semi-definite"
+    if pos == 0:
         return "Negative semi-definite"
     return "Indefinite"
+
